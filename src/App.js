4d1b042a17c22698fc5a877da9da07dfdd82d5ac
Ramazan Doğan <doganrmzn40@gmail.com>
2022-11-12 12:22:10 +0300

    a few changes
    a few changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
-import "./App.css";
-import UserDetail from "./components/UserDetail";
-import UserList from "./components/UserList";
+import './App.css';
+
+import React, { useState } from 'react';
+
+import UserDetail from './components/UserDetail';
+import UserList from './components/UserList';
 
 function App() {
-  const [activeUserId, setActiveUserId] = useState(null);
-
-  return (
-    <div className="App">
-      <div>
-        <UserList setActiveUserId={setActiveUserId} />
+   const [activeUserId, setActiveUserId] = useState(null);
+   return (
+      <div className="App">
+         <div>
+            <UserList setActiveUserId={setActiveUserId} />
+         </div>
+         {activeUserId && (
+            <div>
+               <UserDetail activeUserId={activeUserId} />
+            </div>
+         )}
       </div>
-      {activeUserId && (
-        <div>
-          <UserDetail activeUserId={activeUserId} />
-        </div>
-      )}
-    </div>
-  );
+   );
 }
 
 export default App;
