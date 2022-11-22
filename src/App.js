08676f03ff47c08fe759899e98b2011d71c9c1d5
Ramazan Doğan <doganrmzn40@gmail.com>
2022-11-22 22:28:48 +0300

    All errors are fixed
    All errors are fixed but i'm started completely zero :) but its okay, this is the only way to i can learn..

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-import Input from './components/Input.js';
-import List from './components/List';
-
 function App() {
-   const [data, setData] = useState('');
+   const [getText, setGetText] = useState('');
+
+   const handleText = (event) => {
+      setGetText(event.target.value);
+
+      console.log(event.target.value);
+   };
+
+   const handleClickText = () => {
+      alert(getText);
+   };
 
    return (
-      <div className="flex justify-center align-middle mt-40 ">
-         <Input setData={setData} data={data} />
-      </div>
+      <>
+         <pre>{JSON.stringify(getText)}</pre>
+         <div className="flex justify-center align-middle mt-40 ">
+            RR
+            <input type="text" className="bg-gray-400" onChange={handleText} />
+            <button onClick={handleClickText}>Click Me</button>
+            <ul>
+               <li>{}</li>
+            </ul>
+         </div>
+      </>
    );
 }
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,53 +1,7 @@
 import React from 'react';
 
-function input(setData, data) {
-   const handleinput = (e) => {
-      setData({
-         ...data,
-         [e.target.name]: e.target.value,
-      });
-   };
-
-   return (
-      <div>
-         <div className="justify-center  items-center m-15 inline-block align-middle bg-gray-200 p-20 ">
-            <div className="havetodos mb-5 p-4 bg-gray-300">
-               You have X todos today
-            </div>
-            <div className="flex border bg-gray-300 hover:bg-gray-200 hover:border-black h-12 ">
-               <input
-                  className="flex border bg-gray-300 hover:bg-gray-200 hover:border-black"
-                  type="text"
-                  name="data"
-                  placeholder="Add new todos"
-                  onChange={(e) => setData(e.target.value)}
-                  value={data}
-               />
-               <button
-                  onClick={handleinput}
-                  className="bg-red-500 border border-black text-2xl hover:bg-transparent rounded p-3"
-               >
-                  +
-               </button>
-            </div>
-            <div className="mt-10 flex ">
-               <ul className="">
-                  <li>123</li>
-                  <li>123</li>
-                  <li>123</li>
-                  <li>123</li>
-                  <li>123</li>
-                  <button className="bg-gray-300 p-4 mr-1  hover:bg-gray-400 rounded">
-                     edit
-                  </button>
-                  <button className="bg-gray-300 p-4 mr-1 hover:bg-gray-400 rounded">
-                     erase
-                  </button>
-               </ul>
-            </div>
-         </div>
-      </div>
-   );
+function input() {
+   return <div>as</div>;
 }
 
 export default input;
